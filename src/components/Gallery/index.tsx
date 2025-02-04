834883b28f34e5b8c2886eee6e9571894e97a8e8
Lucas <lucas.ezequiel82@gmail.com>
2025-02-03 22:14:49 -0300

    Projeto segunda parte finalizada

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,120 @@
+import { useState } from 'react'
+
+import { GalleryItem } from '../../pages/Home'
+import { Item, Items, Action, Modal, ModalContent } from './styles'
+import Section from '../Section'
+
+import spider from '../../assets/images/banner-homem-aranha.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: spider
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=HZEaZejwy8QxO-9O'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+  itens: GalleryItem[]
+}
+
+interface ModalState extends GalleryItem {
+  isVisible: boolean
+}
+
+const Gallery = ({ defaultCover, name, itens }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: ''
+    })
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {itens.map((media, index) => (
+            <Item
+              key={media.url}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
+              <img
+                src={getMediaCover(media)}
+                alt={`Midia ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a midia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal className={modal.isVisible ? 'visible' : ''}>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img
+              src={fechar}
+              alt="Icone de fechar"
+              onClick={() => {
+                closeModal()
+              }}
+            />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} />
+          ) : (
+            <iframe frameBorder={0} src={modal.url} />
+          )}
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => {
+            closeModal()
+          }}
+        ></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
---
+++ src/styles.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+  branco: '#EEE',
+  preto: '#111',
+  cinza: '#333',
+  verde: '#10AC84',
+  cinzaClaro: '#a3a3a3'
+}
+
+export const GlobalCss = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    background-color: ${cores.preto};
+    color: ${cores.branco};
+    padding-top: 40px;
+  }
+
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
+`
